0187d4ff4d9da6d002014364451f4ad20e3d4071
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2021-06-03 22:17:45 +0700

    fix unreal macro indent

--- src/Elders.VSE-FormatDocumentOnSave/DocumentFormatService.cs
+++ src/Elders.VSE-FormatDocumentOnSave/DocumentFormatService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using Elders.VSE_FormatDocumentOnSave.Configurations;
 using EnvDTE;
 
@@ -6,6 +8,7 @@ namespace Elders.VSE_FormatDocumentOnSave
 {
     public class DocumentFormatService
     {
+        private static readonly Regex MACRO_REGEX = new Regex(@"^(?<leadingWhitespace>[\s]*)(UPROPERTY|UFUNCTION)\(.*");
         private readonly DTE dte;
         readonly Func<Document, IConfiguration> getGeneralCfg;
         readonly IDocumentFormatter formatter;
@@ -36,6 +39,43 @@ namespace Elders.VSE_FormatDocumentOnSave
                         formatter.Format(doc, filter, commandName);
                     }
                     catch (Exception) { }   // may be we can log which command has failed and why
+                }
+
+                FileInfo fileInfo = new FileInfo(doc.FullName);
+                if (fileInfo.Extension == ".h")
+                {
+                    var dte = doc.DTE;
+                    dte.UndoContext.Open("FixUnrealEngineMacroIndents");
+
+                    try
+                    {
+                        var textDoc = (TextDocument)dte.ActiveDocument.Object("TextDocument");
+                        var underMacro = false;
+                        string leadingWhitespace = null;
+
+                        for (var editPoint = textDoc.StartPoint.CreateEditPoint(); !editPoint.AtEndOfDocument; editPoint.LineDown())
+                        {
+                            if (!underMacro)
+                            {
+                                var lineNum = editPoint.Line;
+                                var line = editPoint.GetLines(lineNum, lineNum + 1);
+                                var macroMatch = MACRO_REGEX.Match(line);
+                                underMacro = macroMatch.Success;
+                                leadingWhitespace = macroMatch.Groups["leadingWhitespace"].ToString();
+                            }
+                            else
+                            {
+                                editPoint.StartOfLine();
+                                editPoint.DeleteWhitespace(EnvDTE.vsWhitespaceOptions.vsWhitespaceOptionsHorizontal);
+                                editPoint.Insert(leadingWhitespace);
+                                underMacro = false;
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        dte.UndoContext.Close();
+                    }
                 }
             }
             catch (Exception) { }   // Do not do anything here on purpose.
